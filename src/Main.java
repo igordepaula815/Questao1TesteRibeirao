b757d19907eeb7cd5f6e1e7680c1802cb1a73f45
igordepaula815 <igordepaula815@gmail.com>
2024-03-13 12:22:29 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int INDICE = 13;
+        int K = 0;
+        int SOMA = 0;
+
+        while ( K < INDICE) {
+            K++;
+            SOMA += K ;
+        }
+        System.out.println( " A SOMA É " + SOMA);
+
+
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
